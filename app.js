7e09d3c1a790dcc2852973cf0af92f51e6cf78ab
YendisFish <brintonsid@gmail.com>
2023-10-20 21:43:52 -0600

    update

--- app.js
+++ app.js
@@ -1,44 +1,110 @@
 particlesJS('particles-js', {
-    particles: {
-      number: {
-        value: 50,  // Number of particles
-        density: {
-          enable: true,
-          value_area: 400  // Adjust the density of particles
-        },
-      },
-      opacity: {
-        value: 0.3 // Adjust the opacity of the particles
-      },
-      color: {
-        value: '#24343d'  // Color of the particles
-      },
-      shape: {
-        type: 'polygon',  // Shape of the particles (circle, edge, triangle, polygon, etc.)
-      },
-      size: {
-        value: 3,  // Size of the particles
-        random: true,  // Enable random size
-      },
-      move: {
+  particles: {
+    number: {
+      value: 95,
+      density: {
         enable: true,
-        speed: 4  // Speed of particle movement
-      },
+        value_area: 800
+      }
     },
-    interactivity: {
-      detect_on: 'canvas',
-      events: {
-        onclick: {
-          enable: true,
-          mode: 'repulse'
-        },
+    color: {
+      value: "#dac5e0"
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000"
       },
+      polygon: {
+        nb_sides: 7
+      },
+      image: {
+        src: "img/github.svg",
+        width: 100,
+        height: 100
+      }
+    },
+    opacity: {
+      value: 0,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false
+      }
+    },
+    size: {
+      value: 1,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false
+      }
     },
     line_linked: {
-        enable: true,
-        distance: 150,
-        color: "#24343d",
-        opacity: 0.4,
-        width: 1
+      enable: false,
+      distance: 150,
+      color: "#7b9ced",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 9.469921162906312,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200
       }
-  });
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "bubble"
+      },
+      onclick: {
+        enable: true,
+        mode: "push"
+      },
+      resize: true
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1
+        }
+      },
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 8,
+        speed: 3
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4
+      },
+      push: {
+        particles_nb: 4
+      },
+      remove: {
+        particles_nb: 2
+      }
+    }
+  },
+  retina_detect: true
+});
